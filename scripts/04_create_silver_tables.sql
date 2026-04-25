b20fe09b586bce5e0346e2a431bdd96945e8c887
Chinmay Pisu <99528603+chinmayc4@users.noreply.github.com>
2026-04-26 01:16:09 +0530

    Update 04_create_silver_tables.sql

--- scripts/04_create_silver_tables.sql
+++ scripts/04_create_silver_tables.sql
@@ -41,95 +41,119 @@ Usage:
     Execute this script after Bronze layer setup to initialize Silver tables.
 ===============================================================================
 */
---Create all required tables for silver layer
 
-IF OBJECT_ID('silver.crm_users','U') IS NOT NULL
-	DROP TABLE silver.crm_users;
--- Create crm_user table
+------------------------------------------------------------
+-- Create all required tables for Silver layer
+------------------------------------------------------------
+
+------------------------------------------------------------
+-- silver.crm_users
+------------------------------------------------------------
+IF OBJECT_ID('silver.crm_users', 'U') IS NOT NULL
+    DROP TABLE silver.crm_users;
+
 CREATE TABLE silver.crm_users (
-	user_id INT,
-	name NVARCHAR(100),
-	email NVARCHAR(100),
-	signup_date DATE,
-	country NVARCHAR(50),
-	device_type NVARCHAR(50),
-	acquisition_channel NVARCHAR(50),
-	dwh_create_time DATETIME2 DEFAULT GETDATE()
+    user_id INT,
+    name NVARCHAR(100),
+    email NVARCHAR(100),
+    signup_date DATE,
+    country NVARCHAR(50),
+    device_type NVARCHAR(50),
+    acquisition_channel NVARCHAR(50),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.crm_user_activity','U') IS NOT NULL
-	DROP TABLE silver.crm_user_activity;
--- Create crm_user_activity
+------------------------------------------------------------
+-- silver.crm_user_activity
+------------------------------------------------------------
+IF OBJECT_ID('silver.crm_user_activity', 'U') IS NOT NULL
+    DROP TABLE silver.crm_user_activity;
+
 CREATE TABLE silver.crm_user_activity (
-	event_id INT,
-	user_id INT,
-	event_type NVARCHAR(50),
-	event_time DATETIME2,
-	device NVARCHAR(50),
-	dwh_create_time DATETIME2 DEFAULT GETDATE()
+    event_id INT,
+    user_id INT,
+    event_type NVARCHAR(50),
+    event_time DATETIME2,
+    device NVARCHAR(50),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.erp_fees','U') IS NOT NULL
-	DROP TABLE silver.erp_fees;
---Create erp_fees
+------------------------------------------------------------
+-- silver.erp_fees
+------------------------------------------------------------
+IF OBJECT_ID('silver.erp_fees', 'U') IS NOT NULL
+    DROP TABLE silver.erp_fees;
+
 CREATE TABLE silver.erp_fees (
-	txn_id INT,
-	processing_fee DECIMAL(18,2),
-	service_fee DECIMAL(18,2),
-	fx_fee DECIMAL(18,2),
-	total_fee DECIMAL(18,2),
-	dwh_create_time DATETIME2 DEFAULT GETDATE()
+    txn_id INT,
+    processing_fee DECIMAL(18,2),
+    service_fee DECIMAL(18,2),
+    fx_fee DECIMAL(18,2),
+    total_fee DECIMAL(18,2),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.erp_fraud_signals','U') IS NOT NULL
-	DROP TABLE silver.erp_fraud_signals;
---Create erp_fraud_signals
+------------------------------------------------------------
+-- silver.erp_fraud_signals
+------------------------------------------------------------
+IF OBJECT_ID('silver.erp_fraud_signals', 'U') IS NOT NULL
+    DROP TABLE silver.erp_fraud_signals;
+
 CREATE TABLE silver.erp_fraud_signals (
-	txn_id INT,
-	ip_address NVARCHAR(50),
-	location NVARCHAR(50),
-	device_id NVARCHAR(50),
-	risk_score DECIMAL(5,3),
-	dwh_create_time DATETIME2 DEFAULT GETDATE()
+    txn_id INT,
+    ip_address NVARCHAR(50),
+    location NVARCHAR(50),
+    device_id NVARCHAR(50),
+    risk_score DECIMAL(5,3),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.erp_merchants','U') IS NOT NULL
-	DROP TABLE silver.erp_merchants;
---Create erp_merchants
+------------------------------------------------------------
+-- silver.erp_merchants
+------------------------------------------------------------
+IF OBJECT_ID('silver.erp_merchants', 'U') IS NOT NULL
+    DROP TABLE silver.erp_merchants;
+
 CREATE TABLE silver.erp_merchants (
-	merchant_id INT,	
-	merchant_name NVARCHAR(50),	
-	category NVARCHAR(50),	
-	country	NVARCHAR(50),
-	onboard_date DATE,	
-	risk_score DECIMAL(5,3),
-	dwh_create_time DATETIME2 DEFAULT GETDATE()
+    merchant_id INT,
+    merchant_name NVARCHAR(50),
+    category NVARCHAR(50),
+    country NVARCHAR(50),
+    onboard_date DATE,
+    risk_score DECIMAL(5,3),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.erp_refunds','U') IS NOT NULL
-	DROP TABLE silver.erp_refunds;
---Create erp_refunds
+------------------------------------------------------------
+-- silver.erp_refunds
+------------------------------------------------------------
+IF OBJECT_ID('silver.erp_refunds', 'U') IS NOT NULL
+    DROP TABLE silver.erp_refunds;
+
 CREATE TABLE silver.erp_refunds (
-	refund_id INT,	
-	txn_id	INT,
-	refund_amount DECIMAL(18,2),	
-	refund_date	DATETIME2,
-	reason NVARCHAR(50),
-	dwh_create_time DATETIME2 DEFAULT GETDATE()
+    refund_id INT,
+    txn_id INT,
+    refund_amount DECIMAL(18,2),
+    refund_date DATETIME2,
+    reason NVARCHAR(50),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.erp_transactions','U') IS NOT NULL
-	DROP TABLE silver.erp_transactions;
---Create erp_transactions
+------------------------------------------------------------
+-- silver.erp_transactions
+------------------------------------------------------------
+IF OBJECT_ID('silver.erp_transactions', 'U') IS NOT NULL
+    DROP TABLE silver.erp_transactions;
+
 CREATE TABLE silver.erp_transactions (
-	txn_id INT,
-	user_id	INT,
-	txn_date DATETIME2, 	
-	amount DECIMAL(18,2),	
-	currency NVARCHAR(50),	
-	payment_method NVARCHAR(50),
-	merchant_id	INT,
-	status	NVARCHAR(50),
-	fraud_flag BIT,
-	dwh_create_time DATETIME2 DEFAULT GETDATE()
+    txn_id INT,
+    user_id INT,
+    txn_date DATETIME2,
+    amount DECIMAL(18,2),
+    currency NVARCHAR(50),
+    payment_method NVARCHAR(50),
+    merchant_id INT,
+    status NVARCHAR(50),
+    fraud_flag BIT,
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
