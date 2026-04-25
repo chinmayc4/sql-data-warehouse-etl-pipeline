67dca33c2a9269fa9ab5c406969a0a5aaa5e8cbb
Chinmay Pisu <99528603+chinmayc4@users.noreply.github.com>
2026-04-25 15:58:01 +0530

    Create 04_create_silver_tables.sql

---
+++ scripts/04_create_silver_tables.sql
@@ -0,0 +1,85 @@
+--Create all required tables for silver layer
+
+IF OBJECT_ID('silver.crm_users','U') IS NOT NULL
+	DROP TABLE silver.crm_users;
+-- Create crm_user table
+CREATE TABLE silver.crm_users (
+	user_id INT,
+	name NVARCHAR(50),
+	email NVARCHAR(50),
+	signup_date DATE,
+	country NVARCHAR(50),
+	device_type NVARCHAR(50),
+	acquisition_channel NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.crm_user_activity','U') IS NOT NULL
+	DROP TABLE silver.crm_user_activity;
+-- Create crm_user_activity
+CREATE TABLE silver.crm_user_activity (
+	event_id INT,
+	user_id NVARCHAR(50),
+	event_type NVARCHAR(50),
+	event_time DATETIME,
+	device NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.erp_fees','U') IS NOT NULL
+	DROP TABLE silver.erp_fees;
+--Create erp_fees
+CREATE TABLE silver.erp_fees (
+	txn_id INT,
+	processing_fee NVARCHAR(50),
+	service_fee NVARCHAR(50),
+	fx_fee NVARCHAR(50),
+	total_fee NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.erp_fraud_signals','U') IS NOT NULL
+	DROP TABLE silver.erp_fraud_signals;
+--Create erp_fraud_signals
+CREATE TABLE silver.erp_fraud_signals (
+	txn_id INT,
+	ip_address NVARCHAR(50),
+	location NVARCHAR(50),
+	device_id NVARCHAR(50),
+	risk_score NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.erp_merchants','U') IS NOT NULL
+	DROP TABLE silver.erp_merchants;
+--Create erp_merchants
+CREATE TABLE silver.erp_merchants (
+	merchant_id INT,	
+	merchant_name NVARCHAR(50),	
+	category NVARCHAR(50),	
+	country	NVARCHAR(50),
+	onboard_date NVARCHAR(50),	
+	risk_score NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.erp_refunds','U') IS NOT NULL
+	DROP TABLE silver.erp_refunds;
+--Create erp_refunds
+CREATE TABLE silver.erp_refunds (
+	refund_id INT,	
+	txn_id	INT,
+	refund_amount NVARCHAR(50),	
+	refund_date	DATE,
+	reason NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.erp_transactions','U') IS NOT NULL
+	DROP TABLE silver.erp_transactions;
+--Create erp_transactions
+CREATE TABLE silver.erp_transactions (
+	txn_id INT,
+	user_id	INT,
+	txn_date DATE, 	
+	amount NVARCHAR(50),	
+	currency NVARCHAR(50),	
+	payment_method NVARCHAR(50),
+	merchant_id	INT,
+	status	NVARCHAR(50),
+	fraud_flag INT
+);
